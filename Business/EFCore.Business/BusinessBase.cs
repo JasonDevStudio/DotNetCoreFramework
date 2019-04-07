b2da19e6016411d79611e02fb9388b2fc1a8f560
yaojie <jiedev@163.com>
2019-04-07 23:15:00 +0800

    updated

---
+++ Business/EFCore.Business/BusinessBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using EFCore.DataAccess;
+using EFCore.Entities;
+
+namespace EFCore.Business
+{
+    public class BusinessBase<TEntity> where TEntity : EntityBase
+    {
+        public List<TEntity> QueryAll()
+        {
+            using (var efr = new EFHelper<OracleContext>())
+            {
+                var list = efr.QueryAll<TEntity>().ToList();
+                return list;
+            }
+        }
+
+        public List<TEntity> QueryExp(Expression<Func<TEntity, bool>> exp)
+        {
+            using (var efr = new EFHelper<OracleContext>())
+            {
+                var list = efr.QueryExp<TEntity>(exp).ToList();
+                return list;
+            }
+        }
+    }
+}
